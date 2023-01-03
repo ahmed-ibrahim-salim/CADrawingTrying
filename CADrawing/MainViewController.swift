2893ad45c0e2faf59fa43fac30b107f1bf3aecd7
Ahmed ibrahim <48734681+ahmed-ibrahim-salim@users.noreply.github.com>
2023-01-03 15:50:48 +0200

    added method to inject auto scroll

--- CADrawing/MainViewController.swift
+++ CADrawing/MainViewController.swift
@@ -17,7 +17,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        injectAutoScrollAnimation()
     }
 }
 
